3727c6e1333dc2d5b3c4030d74d8918c11d8f7cb
imostoviy <i_mostoviy@ukr.net>
2019-01-09 15:51:51 +0200

    Initial Commit

---
+++ AwesomeCodableProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeCodableProject
+//
+//  Created by Мостовий Ігор on 09.01.19.
+//  Copyright © 2019 Мостовий Ігор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
